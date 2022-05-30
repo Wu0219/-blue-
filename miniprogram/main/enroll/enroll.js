ad4614fdec4e782cabc816b6385d6aac9c99c16c
daphnebai <xiaojing779@126.com>
2022-05-30 11:06:49 +0800

    no message

--- miniprogram/main/enroll/enroll.js
+++ miniprogram/main/enroll/enroll.js
@@ -7,6 +7,9 @@ Page({
    * 页面的初始数据
    */
   data: {
+    phonenumber: "",
+    verifycode: "",
+    username: "",
     sex: [{
       id: 1,
       value: '男'
@@ -51,62 +54,101 @@ Page({
     countTime: 60 //倒计时的时间
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  userphoneinput: function (e) {
+    this.data.phonenumber = e.detail.value
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  verifycodeinput: function (e) {
+    this.data.verifycode = e.detail.value
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  usernameinput: function (e) {
+    this.data.username = e.detail.value
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  goback: function (e) {
+    wx.navigateBack({
+      delta: 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  registnext: function (e) {
+    var that = this;
+    var myreg = /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/;
+    if (that.data.phonenumber == "") {
+      wx.showModal({
+        title: '提示',
+        content: '请输入手机号',
+        showCancel: false,
+        success(res) {}
+      })
+    } else if (that.data.verifycode == "") {
+      wx.showModal({
+        title: '提示',
+        content: '请输入验证码',
+        showCancel: false,
+        success(res) {}
+      })
+    } else if (that.data.username == "") {
+      wx.showModal({
+        title: '提示',
+        content: '请输入用户名',
+        showCancel: false,
+        success(res) {}
+      })
+    } else if (that.data.phonenumber.length != 11) {
+      wx.showModal({
+        title: '提示',
+        content: '手机号长度有误',
+        showCancel: false,
+        success(res) {}
+      })
+    } else if (!myreg.test(that.data.phonenumber)) {
+      wx.showModal({
+        title: '提示',
+        content: '请输入正确的手机号',
+        showCancel: false,
+        success(res) {}
+      })
+    } else {
+      const sex = that.data.sex
+      var flag1 = 0
+      for (let i = 0, len = sex.length; i < len; ++i) {
+        if (sex[i].checked == true) {
+          flag1 = 1
+        }
+      }
+      if (flag1 == 0) {
+        wx.showModal({
+          title: '提示',
+          content: '请选择性别',
+          showCancel: false,
+          success(res) {}
+        })
+      }
+      const identity = that.data.identity
+      var flag2 = 0
+      for (let i = 0, len = identity.length; i < len; ++i) {
+        if (identity[i].checked == true) {
+          flag2 = 1
+        }
+      }
+      if (flag2 == 0) {
+        wx.showModal({
+          title: '提示',
+          content: '请选择注册身份',
+          showCancel: false,
+          success(res) {}
+        })
+      }
+    } 
+    if(that.data.identity[0].checked==true){
+      wx.redirectTo({
+        url: '/main/levelgenerate/levelgenerate',
+      })
+    }
+    else{
+      wx.redirectTo({
+        url: '/main/leveltest/leveltest',
+      })
+    }
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-
   radioChange_sex: function (e) {
     // console.log('radio发生change事件，携带value值为：', e.detail.value)
     const sex = this.data.sex
@@ -242,7 +284,7 @@ Page({
 
   // 按钮事件
   getVerificationCode() {
-    var that = this; 
+    var that = this;
     var countTime = that.data.countTime
     // setInterval() 方法可按照指定的周期（以毫秒计）来调用函数或计算表达式，简单来说就是定时执行
     countDown = setInterval(function () {
@@ -261,5 +303,60 @@ Page({
         })
       }
     }, 1000)
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
 })
--- miniprogram/main/login/login.js
+++ miniprogram/main/login/login.js
@@ -5,7 +5,54 @@ Page({
    * 页面的初始数据
    */
   data: {
+    phonenumber: "",
+    verifycode: "",
+    meunShow: [{
+      isShows: true
+    }],
+    sousuoList: [{
+      id: 1,
+      name: '小学以下'
+    }, {
+      id: 2,
+      name: '小学'
+    }, {
+      id: 3,
+      name: '初中'
+    }, {
+      id: 4,
+      name: '高中'
+    }, {
+      id: 5,
+      name: '大学及以上'
+    }],
 
+    identity: [{
+      id: 1,
+      value: '用户'
+    }, {
+      id: 2,
+      value: '志愿者'
+    }],
+    contantTxt: '发送验证码', //按钮中展示的内容
+    countTime: 60 //倒计时的时间
+  },
+
+  userphoneinput: function (e) {
+    this.data.phonenumber = e.detail.value
+  },
+  verifycodeinput: function (e) {
+    this.data.verifycode = e.detail.value
+  },
+  radioChange_identity: function (e) {
+    const identity = this.data.identity
+    for (let i = 0, len = identity.length; i < len; ++i) {
+      identity[i].checked = identity[i].id == e.detail.value
+    }
+    this.setData({
+      identity
+    })
+    console.log(this.data.identity);
   },
 
   /**
